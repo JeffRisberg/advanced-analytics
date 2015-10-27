6a7290e0a220614f3ca884f702f7e1930d0cd93d
JeffRisberg <jrisberg@aol.com>
2015-10-27 15:53:44 -0700

    created comparable java and scala performance

--- src/main/scala/com/incra/timing/FilterJava.java
+++ src/main/scala/com/incra/timing/FilterJava.java
@@ -11,12 +11,31 @@ public class FilterJava {
         long start = System.currentTimeMillis();
         System.out.println("start");
 
+        new JavaFilterExample();
+        new JavaFilterExample();
+        new JavaFilterExample();
+        new JavaFilterExample();
+        new JavaFilterExample();
+
+        long elapsed = System.currentTimeMillis() - start;
+        System.out.println("Total " + elapsed);
+    }
+}
+
+class JavaFilterExample {
+    public JavaFilterExample() {
+        long start = System.currentTimeMillis();
+
         for (int i = 0; i < 10000; i++) {
             String[] states = {"NY", "CA", "NJ", "OH", "OK", "MA", "TX", "MN", "ORE", "FL", "CT", "PA", "WA", "VA", "ME", "VT", "NH", "NV"};
 
-            List validStates = new ArrayList<String>();
+            List<String> validStates = new ArrayList<String>();
             for (String state : states) {
                 if (state.length() == 2) validStates.add(state);
+            }
+            List<String> hawaii = new ArrayList<String>();
+            for (String state : validStates) {
+                if (state.equals("HI")) hawaii.add(state);
             }
             //System.out.println(validStates.size());
         }
--- src/main/scala/com/incra/timing/FilterScala.scala
+++ src/main/scala/com/incra/timing/FilterScala.scala
@@ -9,31 +9,30 @@ object FilterScala {
 
   def main(args: Array[String]): Unit = {
     val start = System.currentTimeMillis()
-    //println("start")
 
-    var i = 0
-    while (i < 10000) {
-      val states = Array("NY", "CA", "NJ", "OH", "OK", "MA", "TX", "MN", "ORE", "FL", "CT", "PA", "WA", "VA", "ME", "VT", "NH", "NV")
-
-      //val validStates = states.filter { state => state.size == 2 }
-      //println(valueStates.size)
-
-      val validStates = mutable.MutableList[String]()
-
-      if (false) {
-        var j = 0
-        while (j < states.size) {
-          val state = states(j)
-          //if (state.size == 2) validStates += state
-          j = j + 1
-        }
-      }
-      //states.foreach { state =>
-      //  if (state.size == 2) validStates += state
-      //}
-      i = i + 1
-    }
+    var x = new ScalaFilterExample()
+    var y = new ScalaFilterExample()
+    var z = new ScalaFilterExample()
+    var t = new ScalaFilterExample()
+    var w = new ScalaFilterExample()
     val elapsed = System.currentTimeMillis() - start
-    println(elapsed)
+    println("Total " + elapsed)
   }
+}
+
+class ScalaFilterExample[@specialized(Int) T] {
+  val start = System.currentTimeMillis()
+
+  var i = 0
+  while (i < 10000) {
+    val states = List("NY", "CA", "NJ", "OH", "OK", "MA", "TX", "MN", "ORE", "FL", "CT", "PA", "WA", "VA", "ME", "VT", "NH", "NV")
+
+    val validStates = states.withFilter { state => state.size == 2 }
+    val hawaii = validStates.withFilter { state => state == "HI" }
+    //println(hawaii.size)
+
+    i = i + 1
+  }
+  val elapsed = System.currentTimeMillis() - start
+  println(elapsed)
 }
